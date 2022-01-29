6d56dc8047e93e1feaae41c481dc272eed885fad
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-01-29 19:33:41 +0530

    removed unused import

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -3,7 +3,6 @@ package com.tests;
 import com.driver.DriverFactory;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
 
 import java.util.Objects;
