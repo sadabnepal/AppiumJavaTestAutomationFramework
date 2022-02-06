bc2da2eb97b4ad5d1f4c138f34614597d1820720
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-02-06 23:20:20 +0530

    created private constructor

--- src/main/java/com/constants/AppPackages.java
+++ src/main/java/com/constants/AppPackages.java
@@ -1,6 +1,10 @@
 package com.constants;
 
 public class AppPackages {
+
+    private AppPackages() {
+    }
+
     public static final String APP_PACKAGE_SAUCELABS = "com.swaglabsmobileapp";
     public static final String APP_ACTIVITY_SAUCELABS = "com.swaglabsmobileapp.MainActivity";
 }
--- src/main/java/com/constants/PathConstants.java
+++ src/main/java/com/constants/PathConstants.java
@@ -2,6 +2,9 @@ package com.constants;
 
 public class PathConstants {
 
+    private PathConstants() {
+    }
+
     private static final String ROOT_PROJECT_DIR = System.getProperty("user.dir");
     public static final String ANDROID_APK_PATH = ROOT_PROJECT_DIR + "/src/test/resources/app/SauceLabs.apk";
     public static final String PROPERTIES_FILE_PATH = "file:${user.dir}/src/test/resources/configs/config.properties";
