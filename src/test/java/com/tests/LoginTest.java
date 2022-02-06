ae9ad06a6e894d7d0fbf4153760fbd6d914bd21b
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-02-06 23:37:51 +0530

    Update LoginTest.java

--- src/test/java/com/tests/LoginTest.java
+++ src/test/java/com/tests/LoginTest.java
@@ -4,20 +4,19 @@ import com.pages.LoginPage;
 import org.testng.annotations.Test;
 
 import static com.constants.DataConstants.*;
-import static org.testng.Assert.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 public class LoginTest extends BaseTest {
 
     @Test
     public void loginPageLogoTest() {
-        assertTrue(new LoginPage().isAppLogoDisplayed());
+        assertThat(new LoginPage().isAppLogoDisplayed()).isTrue();
     }
 
     @Test(priority = 1)
     public void invalidLoginTest() {
         LoginPage loginPage = new LoginPage();
         loginPage.setUsername(LOCKED_OUT_USER).setPassword(LOGIN_PASSWORD).clickOnLoginBtn();
-        assertEquals(loginPage.getErrorText(), USER_LOCKED_OUT_ERROR);
+        assertThat(loginPage.getErrorText()).isEqualTo(USER_LOCKED_OUT_ERROR);
     }
 
     @Test(priority = 2)
@@ -26,6 +25,6 @@ public class LoginTest extends BaseTest {
                 .setUsername(VALID_USER)
                 .setPassword(LOGIN_PASSWORD)
                 .clickAndNavigateToProduct().isProductHeaderDisplayed();
-        assertTrue(isProductPageDisplayed);
+        assertThat(isProductPageDisplayed).isTrue();
     }
 }
