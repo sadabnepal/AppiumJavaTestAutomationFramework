c5ffbcbc416e005586574f2c36fff4cdb716f2ce
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-02-13 23:23:26 +0530

    updated with file separator

--- src/main/java/com/constants/PathConstants.java
+++ src/main/java/com/constants/PathConstants.java
@@ -1,13 +1,16 @@
 package com.constants;
 
+import java.io.File;
+
 public class PathConstants {
 
     private PathConstants() {
     }
 
     private static final String ROOT_PROJECT_DIR = System.getProperty("user.dir");
-    public static final String ANDROID_APK_PATH = ROOT_PROJECT_DIR + "/src/test/resources/app/SauceLabs.apk";
+    public static final String ANDROID_APK_PATH = ROOT_PROJECT_DIR + File.separator + "src" + File.separator + "test" +
+            File.separator + "resources" + File.separator + "app" + File.separator + "SauceLabs.apk";
     public static final String PROPERTIES_FILE_PATH = "file:${user.dir}/src/test/resources/configs/config.properties";
-    public static final String REPORT_PATH = ROOT_PROJECT_DIR + "/index.html";
+    public static final String REPORT_PATH = ROOT_PROJECT_DIR + File.separator + "index.html";
 
 }
