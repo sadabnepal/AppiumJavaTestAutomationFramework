235540c386a98ea18197252b065e1923603f21d8
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-02-24 23:23:32 +0530

    Framework optimization |  wait implementation

--- src/main/java/com/pages/BasePage.java
+++ src/main/java/com/pages/BasePage.java
@@ -1,7 +1,5 @@
 package com.pages;
 
-
-import com.driver.DriverManager;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.android.AndroidDriver;
@@ -12,9 +10,12 @@ import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static com.driver.DriverManager.*;
 import static com.reports.ExtentLogger.*;
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
@@ -23,33 +24,35 @@ import static io.appium.java_client.touch.offset.ElementOption.element;
 public class BasePage {
 
     public BasePage() {
-        PageFactory.initElements(new AppiumFieldDecorator(DriverManager.getDriver()), this);
+        PageFactory.initElements(new AppiumFieldDecorator(getDriver()), this);
     }
 
     protected void clickElement(MobileElement element, String elementName) {
+        waitForElementVisibility(element);
         element.click();
         info(elementName + " is clicked successfully!!");
     }
 
     protected void enterData(MobileElement element, String value, String elementName) {
+        waitForElementVisibility(element);
         element.setValue(value);
         info(value + " is entered successfully in " + elementName);
     }
 
     protected WebElement scrollAndFindElementByText(String text, String elementName) {
         info("Performing scroll and to find " + elementName + " by text");
-        return ((AndroidDriver<?>) DriverManager.getDriver())
+        return ((AndroidDriver<?>) getDriver())
                 .findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + text + "\"))");
     }
 
     protected void tapElement(MobileElement element, String elementName) {
-        new AndroidTouchAction((PerformsTouchActions) DriverManager.getDriver())
+        new AndroidTouchAction((PerformsTouchActions) getDriver())
                 .tap(tapOptions().withElement(element(element))).perform();
         info("Tapped successfully on " + elementName);
     }
 
     protected void longPress(MobileElement peopleNamesMenu, long duration, String elementName) {
-        new AndroidTouchAction((PerformsTouchActions) DriverManager.getDriver())
+        new AndroidTouchAction((PerformsTouchActions) getDriver())
                 .longPress(longPressOptions().withElement(element(peopleNamesMenu)))
                 .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(duration)))
                 .perform();
@@ -57,7 +60,7 @@ public class BasePage {
     }
 
     protected void moveElement(MobileElement source, WebElement target) {
-        new AndroidTouchAction((PerformsTouchActions) DriverManager.getDriver())
+        new AndroidTouchAction((PerformsTouchActions) getDriver())
                 .tap(TapOptions.tapOptions().withElement(ElementOption.element(source)))
                 .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
                 .moveTo(ElementOption.element(target))
@@ -66,9 +69,15 @@ public class BasePage {
     }
 
     protected boolean isElementDisplayed(MobileElement element) {
+        waitForElementVisibility(element);
         boolean state = element.isDisplayed();
         info("Element " + element + " is displayed: " + state);
         return state;
+    }
+
+    protected void waitForElementVisibility(MobileElement element) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), 20);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 
 }
