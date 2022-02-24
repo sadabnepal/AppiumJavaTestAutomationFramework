cc9e91a8e1267523120bf0effd33d220d5d0570b
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-02-24 23:23:52 +0530

    Framework optimization |  more login scenarios

--- src/test/java/com/tests/LoginTest.java
+++ src/test/java/com/tests/LoginTest.java
@@ -15,13 +15,27 @@ public class LoginTest extends BaseTest {
     }
 
     @Test(priority = 1)
-    public void invalidLoginTest() {
+    public void invalidUserTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.setUsername(IN_VALID_USER).setPassword(LOGIN_PASSWORD).clickOnLoginBtn();
+        assertThat(loginPage.getErrorText()).isEqualTo(INVALID_CRED_ERROR);
+    }
+
+    @Test(priority = 2)
+    public void invalidPasswordTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.setUsername(VALID_USER).setPassword(IN_VALID_PASSWORD).clickOnLoginBtn();
+        assertThat(loginPage.getErrorText()).isEqualTo(INVALID_CRED_ERROR);
+    }
+
+    @Test(priority = 3)
+    public void lockedUserTest() {
         LoginPage loginPage = new LoginPage();
         loginPage.setUsername(LOCKED_OUT_USER).setPassword(LOGIN_PASSWORD).clickOnLoginBtn();
         assertThat(loginPage.getErrorText()).isEqualTo(USER_LOCKED_OUT_ERROR);
     }
 
-    @Test(priority = 2)
+    @Test(priority = 4)
     public void validLoginTest() {
         boolean isProductPageDisplayed = new LoginFacade()
                 .navigateToProductPage(VALID_USER, LOGIN_PASSWORD)
